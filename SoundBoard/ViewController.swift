deea60aa338b589f0b46a4a3201a910a28ed441c
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2023-05-24 08:33:44 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Junior on 5/24/23.
+//  Copyright © 2023 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
